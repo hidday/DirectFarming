f3888585477fec4fd59a07a172d9050ccdda06ab
avrister <avri9@hotmail.com>
2015-02-27 17:15:14 +0200

    from avri 17:45

--- app/src/main/java/com/example/hidday/directfarming/MainActivity.java
+++ app/src/main/java/com/example/hidday/directfarming/MainActivity.java
@@ -7,19 +7,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.parse.Parse;
+import com.parse.ParseACL;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseUser;
+
 
 public class MainActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Add your initialization code here
+        Parse.initialize(this, "zlDQOcXVpC2a3exWmuvGl3FcWGvsmpn0MgFC7i1D", "I29kas1FRUnzesQxtqegGlpBEoHUDMhmft5QtCDY");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+
+
         // Determine whether the current user is an anonymous user
         if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
-            // If user is anonymous, send the user to LoginSignupActivity.class
-            Intent intent = new Intent(MainActivity.this,
-                    LoginActivity.class);
+            // If user is anonymous, send the user to LoginActivity.class
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
         } else {
